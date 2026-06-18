b21b34ab7939d71c0fc9b3b73f8f22551fa30793
SergioemiliodaCunha <sergioemiliocunha@gmail.com>
2026-06-18 00:24:57 -0300

    correção

--- sql/queries/consultas.sql
+++ sql/queries/consultas.sql
@@ -4,7 +4,7 @@ FROM Aluno a
 JOIN Assinatura s ON a.id_aluno = s.fk_Aluno_id_aluno
 WHERE s.status = 'inadimplente'
 AND YEAR(s.data_inicio) = 2025;
-Resultado 6 – Resposta da Consulta 1 em SQL
+
 
 2. Qual a quantidade de assinaturas realizadas por tipo de plano (mensal, trimestral, semestral e diária) em 2025?
 sqlSELECT p.tipo, COUNT(s.id_assinatura) AS total_assinaturas
@@ -12,21 +12,21 @@ FROM Assinatura s
 JOIN Plano p ON s.fk_Plano_id_plano = p.id_plano
 WHERE YEAR(s.data_inicio) = 2025
 GROUP BY p.tipo;
-Resultado 7 – Resposta da Consulta 2 em SQL
+
 
 3. Qual o total arrecadado de pagamentos realizados em 2025, agrupados por forma de pagamento?
 sqlSELECT forma_pagamento, SUM(valor) AS total_arrecadado
 FROM Pagamento
 WHERE YEAR(data_pagamento) = 2025
 GROUP BY forma_pagamento;
-Resultado 8 – Resposta da Consulta 3 em SQL
+
 
 4. Qual a quantidade de avaliações físicas por status da avaliação (pendente ou concluída) em 2025?
 sqlSELECT status_avaliacao, COUNT(*) AS total
 FROM Avaliacao_Fisica
 WHERE YEAR(data) = 2025
 GROUP BY status_avaliacao;
-Resultado 9 – Resposta da Consulta 4 em SQL
+
 
 5. Quais são os 10 alunos com maior frequência de acesso à academia durante o ano de 2025?
 sqlSELECT a.id_aluno, a.nome, COUNT(ac.id_acesso) AS frequencia
